c3c641905930c9a1e5f5b1d8d1d93609f4b7160c
Kiwoong Kim <kiwoong.kim@gachidata.com>
2020-06-19 14:44:53 +0900

    Spring DI test #2 - setter 방식으로 DI구현

--- webm01/src/test/java/com/gachidata/di/MessageRenderer.java
+++ webm01/src/test/java/com/gachidata/di/MessageRenderer.java
@@ -1,15 +1,29 @@
 package com.gachidata.di;
 
 public class MessageRenderer {
+	private MessageProvider messageProvider; // setter di
+	
+	public void setMessageProvider(MessageProvider messageProvider) {
+		this.messageProvider = messageProvider;
+	}
+	
 	public void render() {
 		//MessageProvider mp = new HelloWorldMessageProvider();
-		MessageProvider mp = new HiWorldMessageProvider();
-		System.out.println(mp.getMessage());
+		//MessageProvider mp = new HiWorldMessageProvider();
+		//System.out.println(mp.getMessage());
+		System.out.println(messageProvider.getMessage()); 
 	}
 
 	public static void main(String[] args) {
 		MessageRenderer messageRenderer = new MessageRenderer();
+		
+		// setter 방식으로 provider를 변경
+		messageRenderer.setMessageProvider(new HelloWorldMessageProvider());
 		messageRenderer.render();
+		
+		messageRenderer.setMessageProvider(new HiWorldMessageProvider());
+		messageRenderer.render();
+		
 	}
 
 }
