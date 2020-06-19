9d35641fe594cfaa46d21b80badb710ee08661e3
Kiwoong Kim <kiwoong.kim@gachidata.com>
2020-06-19 14:55:26 +0900

    Spring DI test #4 - Spring Configuration File (di.xml) 사용

--- webm01/src/test/java/com/gachidata/di/MessageRenderer.java
+++ webm01/src/test/java/com/gachidata/di/MessageRenderer.java
@@ -1,5 +1,8 @@
 package com.gachidata.di;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MessageRenderer {
 	private MessageProvider messageProvider; // setter di
 	
@@ -15,14 +18,19 @@ public class MessageRenderer {
 	}
 
 	public static void main(String[] args) {
-		MessageRenderer messageRenderer = new MessageRenderer();
+//		MessageRenderer messageRenderer = new MessageRenderer();
+//		
+//		// setter 방식으로 provider를 변경
+//		messageRenderer.setMessageProvider(new HelloWorldMessageProvider());
+//		messageRenderer.render();
+//		
+//		messageRenderer.setMessageProvider(new HiWorldMessageProvider());
+//		messageRenderer.render();
 		
-		// setter 방식으로 provider를 변경
-		messageRenderer.setMessageProvider(new HelloWorldMessageProvider());
-		messageRenderer.render();
-		
-		messageRenderer.setMessageProvider(new HiWorldMessageProvider());
-		messageRenderer.render();
+		// di.xml 사용
+		ApplicationContext ac = new ClassPathXmlApplicationContext("di.xml");
+		MessageRenderer renderer = (MessageRenderer)ac.getBean("messageRenderer");
+		renderer.render();
 		
 	}
 
